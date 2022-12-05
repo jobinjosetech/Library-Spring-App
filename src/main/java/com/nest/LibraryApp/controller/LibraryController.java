c15395b74304900f023ea26bd4e732410a620075
jobin <jobinjose.in@gmail.com>
2022-12-05 23:42:17 +0530

    View Book

--- src/main/java/com/nest/LibraryApp/controller/LibraryController.java
+++ src/main/java/com/nest/LibraryApp/controller/LibraryController.java
@@ -27,7 +27,7 @@ public class LibraryController {
         return "Welcome to Add Book";
     }
 
-    @PostMapping("/viewBook")
+    @GetMapping("/viewBook")
     public String ViewBook(){
         return "Welcome to View Book";
     }
