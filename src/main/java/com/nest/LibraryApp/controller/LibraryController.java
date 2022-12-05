44b00e83015889c8ef07d730124eefd649394924
jobin <jobinjose.in@gmail.com>
2022-12-05 23:25:29 +0530

    LibraryApp

---
+++ src/main/java/com/nest/LibraryApp/controller/LibraryController.java
@@ -0,0 +1,15 @@
+package com.nest.LibraryApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryController {
+
+    @PostMapping("/")
+    public String AdminLogin(){
+        return "Welcome to admin Login";
+    }
+
+}
